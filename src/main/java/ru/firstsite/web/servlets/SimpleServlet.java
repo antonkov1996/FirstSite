0f1ee0c9116908442dc4298cf9e12dcecce9de72
antonkov1996 <antonkov1996@gmail.com>
2017-08-25 18:51:43 +0300

    JSP

--- src/main/java/ru/firstsite/web/servlets/SimpleServlet.java
+++ src/main/java/ru/firstsite/web/servlets/SimpleServlet.java
@@ -5,11 +5,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setCharacterEncoding("Windows-1251");
-        resp.getWriter().write("ok12 привет");
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(i+1);
+        }
+        req.setAttribute("list1",list);
+        req.setAttribute("name","Vasia");
+        req.getRequestDispatcher("/WEB-INF/jsps/page.jsp").forward(req,resp);
     }
 }
--- web/WEB-INF/jsps/page.jsp
+++ web/WEB-INF/jsps/page.jsp
@@ -2,15 +2,23 @@
   Created by IntelliJ IDEA.
   User: specops27
   Date: 25.8.17
-  Time: 17.27
+  Time: 18.19
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+jsp
+<hr>
+${param.city}
+<hr>
+${requestScope.name}
+
+фывфыв
+<hr>
+${requestScope.list1}
+</body>
 </html>

7564dad7209607f33c3a0d15cc54490e40be5cad
antonkov1996 <antonkov1996@gmail.com>
2017-08-27 17:25:24 +0300

    Hibernate

---
+++ src/main/java/ru/firstsite/web/servlets/TestTableEntity.java
@@ -0,0 +1,50 @@
+package ru.firstsite.web.servlets;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "test_table", schema = "test_for_site", catalog = "")
+public class TestTableEntity {
+    private int id;
+    private String text;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "text")
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TestTableEntity that = (TestTableEntity) o;
+
+        if (id != that.id) return false;
+        if (text != null ? !text.equals(that.text) : that.text != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        return result;
+    }
+}
